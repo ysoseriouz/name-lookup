cada2f34e23c41613e817a6e9e6f53c559a9fa37
Thanh NT <thanh.nguyen.ntt2612@gmail.com>
2025-02-20 21:16:11 +0700

    improve tracing + refactor

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,8 @@ mod tls;
 
 use anyhow::{Context, Result};
 use axum::{
-    body::Bytes,
-    extract::{MatchedPath, State},
-    http::{HeaderMap, Request},
+    extract::State,
+    http::Request,
     response::{IntoResponse, Response},
     routing::{get, post},
     Router,
@@ -15,27 +14,21 @@ use initializer::{initialize, AppState};
 use std::{net::SocketAddr, time::Duration};
 use tls::{build_tls_config, redirect_http_to_https, Ports};
 use tokio::signal;
+use tower::ServiceBuilder;
 use tower_http::{
     classify::ServerErrorsFailureClass, compression::CompressionLayer,
     decompression::RequestDecompressionLayer, services::ServeDir, timeout::TimeoutLayer,
     trace::TraceLayer,
 };
 use tracing::{debug, error, info, info_span, Span};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Tracing
     tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!(
-                    "{}=debug,tower_http=debug,axum::rejection=trace",
-                    env!("CARGO_CRATE_NAME")
-                )
-                .into()
-            }),
-        )
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            format!("{}=trace,tower_http=trace", env!("CARGO_CRATE_NAME")).into()
+        }))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
@@ -45,39 +38,7 @@ async fn main() -> Result<()> {
     };
     let tls_config = build_tls_config()?;
     let app_state = initialize().await?;
-    let trace_layer = TraceLayer::new_for_http()
-        .make_span_with(|req: &Request<_>| {
-            let matched_path = req
-                .extensions()
-                .get::<MatchedPath>()
-                .map(MatchedPath::as_str);
-
-            info_span!(
-                "http_request",
-                method = ?req.method(),
-                matched_path,
-                some_other_field = tracing::field::Empty
-            )
-        })
-        .on_request(|_req: &Request<_>, _span: &Span| {})
-        .on_response(|_res: &Response, _latency: Duration, _span: &Span| {})
-        .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {})
-        .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {})
-        .on_failure(|_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {});
-    let timeout_layer = TimeoutLayer::new(Duration::from_secs(10));
-    let app = Router::new()
-        .route("/", get(html_template::lookup::index))
-        .route("/_chk", get(health_check))
-        .route("/lookup", post(html_template::lookup::add_name))
-        .route("/joke", get(html_template::joke::index))
-        .route("/joke/renew", get(html_template::joke::renew))
-        .nest_service("/static", ServeDir::new("static"))
-        .layer(trace_layer)
-        .layer(timeout_layer)
-        .layer(RequestDecompressionLayer::new())
-        .layer(CompressionLayer::new())
-        .with_state(app_state.clone())
-        .fallback(handler_404);
+    let app = app(app_state.clone());
     let addr = SocketAddr::from(([0, 0, 0, 0], ports.https));
     let handle = axum_server::Handle::new();
     let shutdown_future = shutdown_signal(handle.clone(), app_state.clone());
@@ -92,6 +53,50 @@ async fn main() -> Result<()> {
         .context("Error while starting server")?;
 
     Ok(())
+}
+
+fn app(app_state: AppState) -> Router {
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(|req: &Request<_>| {
+            info_span!(
+                "request",
+                method = ?req.method(),
+                uri = ?req.uri(),
+                version = ?req.version(),
+                status_code = tracing::field::Empty,
+            )
+        })
+        .on_request(|req: &Request<_>, _span: &Span| {
+            info!("📥 Request: {} {}", req.method(), req.uri());
+        })
+        .on_response(|res: &Response<_>, latency: Duration, span: &Span| {
+            let status = res.status().as_u16();
+            span.record("status_code", status);
+
+            info!("✅ Response: {} | Latency: {:?}", status, latency);
+        })
+        .on_failure(
+            |error: ServerErrorsFailureClass, latency: Duration, _span: &Span| {
+                error!("❌ Request failed after {:?}: {:?}", latency, error);
+            },
+        );
+    let timeout_layer = TimeoutLayer::new(Duration::from_secs(10));
+    let service_layer = ServiceBuilder::new()
+        .layer(trace_layer)
+        .layer(timeout_layer)
+        .layer(RequestDecompressionLayer::new())
+        .layer(CompressionLayer::new());
+
+    Router::new()
+        .route("/", get(html_template::lookup::index))
+        .route("/_chk", get(health_check))
+        .route("/lookup", post(html_template::lookup::add_name))
+        .route("/joke", get(html_template::joke::index))
+        .route("/joke/renew", get(html_template::joke::renew))
+        .nest_service("/static", ServeDir::new("static"))
+        .layer(service_layer)
+        .with_state(app_state.clone())
+        .fallback(handler_404)
 }
 
 async fn health_check() -> impl IntoResponse {
